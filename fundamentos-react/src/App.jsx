401eb8d72931f2c852bd35283894f0ab65cc8c9a
Guilherme Brandão <guilherme.brandao@pitang.com>
2021-04-30 19:15:30 -0300

    Componente Card #01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -8,12 +9,25 @@ import Fragmento from './components/basicos/Fragmento'
 export default _ =>
   <div id="app">
     <h2>Fundamentos React</h2>
-    <Aleatorio
-      min={1}
-      max={60} />
-    <Fragmento />
-    <ComParametro
-      titulo="Segundo Componente"
-      subtitulo="Muito legal!!" />
-    <Primeiro />
+
+    <Card
+      titulo="Aleatorio">
+      <Aleatorio
+        min={1}
+        max={60} />
+    </Card>
+
+    <Card titulo="Fragmento">
+      <Fragmento />
+    </Card>
+
+    <Card titulo="ComParametro">
+      <ComParametro
+        titulo="Segundo Componente"
+        subtitulo="Muito legal!!" />
+    </Card>
+
+    <Card titulo="ComParametro">
+      <Primeiro />
+    </Card>
   </div>
