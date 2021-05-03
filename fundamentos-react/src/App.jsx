208ffee98c90c1a1d57420c3ceba9aac66c14644
Guilherme Brandão <guilherme.brandao@pitang.com>
2021-05-03 09:10:06 -0300

    Renderizaçao Condicional #01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -10,19 +10,27 @@ import Familia from './components/basicos/Familia'
 import FamiliaMembro from './components/basicos/FamiliaMembro'
 import ListaAlunos from './components/repeticao/ListaAlunos'
 import TabelaProdutos from './components/repeticao/TabelaProdutos'
+import ParOuImpar from './components/condicional/ParOuImpar'
 
 export default _ =>
   <div id="app">
     <h2>Fundamentos React</h2>
 
     <div className="Cards">
-    <Card
+
+      <Card
+        titulo="#08 - Renderização Condicional"
+        color="#982395">
+        <ParOuImpar numero={20}/>
+      </Card>
+
+      <Card
         titulo="#07 - Tabela Produtos"
         color="#3A9AD9">
         <TabelaProdutos />
       </Card>
 
-    <Card
+      <Card
         titulo="#06 - Repeticao"
         color="#FF4C65">
         <ListaAlunos />
@@ -32,8 +40,8 @@ export default _ =>
         titulo="#05 - Familia"
         color="#00C8F8">
         <Familia sobrenome="Silva">
-          <FamiliaMembro nome="Abigail"  />
-          <FamiliaMembro nome="Eva"  />
+          <FamiliaMembro nome="Abigail" />
+          <FamiliaMembro nome="Eva" />
           <FamiliaMembro nome="Pedro" />
         </Familia>
       </Card>
