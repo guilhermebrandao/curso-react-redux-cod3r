41054a024976aec5bd6e32333359c2a285e54a55
Guilherme Brandão <guilherme.brandao@pitang.com>
2021-05-10 19:39:42 -0300

    useRef #02

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -5,11 +5,20 @@ import SectionTitle from '../../components/layout/SectionTitle'
 const UseRef = (props) => {
 
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
 
     useEffect(() => {
-        count.current = count.current + 1
+        count.current++
+        myInput2.current.focus()
     }, [value1])
+
+    useEffect(() => {
+        count.current++
+        myInput1.current.focus()
+    }, [value2])
 
     return (
         <div className="UseRef">
@@ -29,8 +38,19 @@ const UseRef = (props) => {
                 </div>
                 <input type="text"
                     className="input"
+                    ref={myInput1}
                     value={value1}
                     onChange={e => setValue1(e.target.value)} />
+            </div>
+
+
+            <SectionTitle title="Exercicio #02" />
+            <div className="center">
+                <input type="text"
+                    className="input"
+                    ref={myInput2}
+                    value={value2}
+                    onChange={e => setValue2(e.target.value)} />
             </div>
         </div>
     )
