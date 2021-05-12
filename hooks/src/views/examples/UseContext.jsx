52480a2eaa1be647f44f927e72322c8e099df067
Guilherme Brandão <guilherme.brandao@pitang.com>
2021-05-11 23:04:39 -0300

    useContext #01

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+
 const UseContext = (props) => {
+
+    const {state, setState} = useContext(DataContext)
+
+    const setNumber = (n) => {
+        setState({
+            ...state,
+            number: n
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <div className="center"></div>
+            <span className="text">{state.text}</span>
+            <span className="text">{state.number}</span>
+
+            <div>
+                <button className="btn" onClick={() => setNumber(state.number-1)}>-1</button>
+                <button className="btn" onClick={() => setNumber(state.number+1)}>+1</button>
+            </div>
         </div>
     )
 }
